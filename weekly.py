6503ce64c7b6145b37ea808f4af6c606a398ee8d
Luke Weigel <lfweigel88@gmail.com>
2020-09-03 13:15:10 -0400

    lweigel

--- weekly.py
+++ weekly.py
@@ -6,10 +6,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("start", type=int, help="Number of days ago to start")
 parser.add_argument("times", type=str, help="Times to grab")
+parser.add_argument("--upload", help="Upload flag", action="store_true")
 args = parser.parse_args()
+print(args.upload)
 startinput = args.start
 timesinput = args.times
-os.system('rm -rf tmp')
+os.system('rm -rf /lemontree/tmp')
 today = datetime.datetime.now()
 year = today.year
 month = today.month
@@ -20,20 +22,32 @@ timesinput = timesinput.split(',')
 times = []
 for i in timesinput:
 	times.append(i)
-start = int(day) - int(startinput)
-print(start)
-print(times)
+#start = int(day) - int(startinput)
+start = datetime.datetime.now() - datetime.timedelta(days=startinput)
+#print(dir(start))
+startda = start.day
+startmo = start.month
 os.system("mkdir /lemontree/tmp")
-while start <= day:
-	if start < 10:
-	        startstr = ("0"+str(start))
+if startmo < 10:
+	startmostr = ("0"+str(startmo))
+else:
+	startmostr = str(startmo)
+i = int(startinput)
+while i >= 0:
+	start = datetime.datetime.now() - datetime.timedelta(days=i)
+	if start.month == month:
+		startmostr = str(month)
+	if start.day < 10:
+	        startdastr = ("0"+str(start.day))
 	else:
-        	startstr=str(start)
-	for i in times:
-		os.system('cp ./2020/08/'+startstr+'/*-'+i+'-00.jpg tmp/')
-	start = start + 1
+        	startdastr=str(start.day)
+	for t in times:
+		print('/lemontree/2020/'+str(startmostr)+'/'+startdastr+'/*-'+t+'-00.jpg /lemontree/tmp/')
+		os.system('cp /lemontree/2020/'+str(startmostr)+'/'+str(startdastr)+'/*-'+str(t)+'-00.jpg /lemontree/tmp/')
+	i = int(i) - 1
 name = "weekly-" + str(year) + "-" + str(month) + "-" + str(day) + "-" + str(today.hour) + "-" + str(today.minute)
 print(name)
-os.system('ffmpeg -framerate 1 -pattern_type glob -i "tmp/*.jpg" -c:v libx264 -pix_fmt yuv420p '+ name + '.mp4')
-#os.system('python /lemontree/youtube.py --file=/lemontree/'+name+'.mp4 --title='+name+' --category="22" --privacyStatus="public" --noauth_local_webserver')
-#os.system('rm -rf '+name+'.mp4')
+os.system('ffmpeg -framerate 1 -pattern_type glob -i "/lemontree/tmp/*.jpg" -c:v libx264 -pix_fmt yuv420p /lemontree/'+ name + '.mp4')
+if args.upload:
+	os.system('cd /lemontree; python /lemontree/youtube.py --file=/lemontree/'+name+'.mp4 --title='+name+' --category="22" --privacyStatus="public" --noauth_local_webserver')
+	os.system('rm -rf /lemontree/'+name+'.mp4')
